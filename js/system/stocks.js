a4f9c24bad24793bfa58e393cd11dfc6f4f52513
Ruswole <norlydude@gmail.com>
2024-05-26 11:24:49 +0800

    patch notes log out on every page

--- js/system/stocks.js
+++ js/system/stocks.js
@@ -320,3 +320,29 @@ const showData = async (id) => {
 //   // Refresh card list
 //   getDatas(url);
 // };
+const btn_logout = document.getElementById("btn_logout");
+btn_logout.onclick = async () => {
+  // Access Logout API Endpoint
+  const response = await fetch(backendURL + "/api/logout", {
+    headers: {
+      Accept: "application/json",
+      Authorization: "Bearer " + localStorage.getItem("token"),
+    },
+  });
+
+  // Get response if 200-299 status code
+  if (response.ok) {
+    // Clear Tokens
+    localStorage.clear();
+
+    successNotification("Logout Successful.");
+    // Redirect Page
+    window.location.pathname = "/";
+  }
+  // Get response if 400 or 500 status code
+  else {
+    const json = await response.json();
+
+    errorNotification(json.message, 10);
+  }
+};
